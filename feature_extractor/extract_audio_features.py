4299eab06ad1f09e0cfeac8a938b62cce883b0de
Sabarish Gopalakrishnan (RIT S <sxg8458@kgcoe-cuda-02.main.ad.rit.edu>
2019-02-16 19:12:41 -0500

    verifying features for audio

--- feature_extractor/extract_audio_features.py
+++ feature_extractor/extract_audio_features.py
@@ -8,6 +8,7 @@ import pdb
 
 
 #Train,dev,test images list
+#Putting all the file names into respective lists
 with open('/shared/kgcoe-research/mil/new_cvs_data/Flickr8k_text/Flickr_8k.trainImages.txt','r') as f:
     train_images = f.read().splitlines()
 print('the total train images are: %s' %len(train_images))
@@ -36,6 +37,8 @@ for index in names:
 print('the total audio files are: %s' %len(audio_list))    
 print('the total images are: %s' %len(image_list))    
 
+
+#sorting all lists to keep them in order
 audio_list= sorted(audio_list)
 image_list= sorted(image_list)
 
@@ -43,32 +46,23 @@ image_list= sorted(image_list)
 index_list = [i for i,x in enumerate(image_list) if x in train_images]
 print('the total train images are: %s' %len(index_list))
 
+#Getting only the training wavs.
 train_audio = []
 for val in index_list:
     train_audio.append(audio_list[val])
 print('the total audio files for training are: %s' %len(train_audio))
-'''
-#Dealing with the captions
-with open('/shared/kgcoe-research/mil/new_cvs_data/Flickr8k_text/Flickr8k.lemma.token.txt','r') as f:
-    captions = f.read().splitlines()
-file_list=[]
-captions_list=[]
-for line in captions:
-    file_list.append(line.split('#')[0])
-    sentence = line.split('#')[1].split('\t')[1]
-    captions_list.append(sentence)
 
-train_sentences = []
-cap_files = []
-for element in train_images:
-    idx_list = [i for i,val in enumerate(file_list) if val==element]
-    for i in idx_list:
-        train_sentences.append(captions_list[i])
-        cap_files.append(file_list[i])
 
-print('the total captions files for training are: %s' %len(train_sentences))
-'''
-
+def remap(arr):
+    old_size = arr.shape[0]#100
+    new_shape = 100#arr.shape[0]
+    new_arr = np.zeros((new_shape,arr.shape[1]))
+    new_idxs = [i*old_size//new_shape + old_size//(2*new_shape) for i in range(new_shape)]
+    for n,idx in enumerate(new_idxs):
+        new_arr[n,:] = arr[idx,:]
+    return new_arr
+    
+    
 def audioToInputVector(audio, fs, numcep, nfilt):
 	# Get MFCC coefficients
 	features = mfcc(audio, samplerate=fs, numcep=numcep, nfilt=nfilt)
@@ -82,14 +76,14 @@ def audiofile_to_input_vector(audio_filename, numcep, nfilt):
     '''
     # Load .wav file
     fs, audio = wav.read(audio_filename)
+    #pdb.set_trace()
     return audioToInputVector(np.int16(audio), fs, numcep, nfilt)
+
 
 #audio_features = audiofile_to_input_vector('/shared/kgcoe-research/mil/Kruthika/AllAugmentedSenecaData25/SJ_DeepSpeech_SJ_deer_habits_23.wav', 29, 29)
 #pdb.set_trace()
 
-def main(args):
-    print("in main")
-    
+def main(args):   
     audio_features_np = np.array([])
     for i in range(0, len(train_audio)):
         if i%500==0 and i!=0: print("Extracted: {}/{}".format(i, (len(train_audio))))
